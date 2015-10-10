96f45aedbcfd578b41f68165eb8b5e87c9c0ab0d
papaq <yuralightman@gmail.com>
2015-10-10 20:41:57 +0300

    test Client. Mock

--- Client.py
+++ Client.py
@@ -6,81 +6,84 @@ client = SoapClient(
     location="http://localhost:8000/")
 
 
-def testf(t):
-    return client.testf(t=t)
+class ClassProduction:
+    def __init__(self):
+        pass
 
+    def testf(self, t):
+        return client.testf(t=t)
 
-def create(key, val):
-    return client.CreateRecord(key=key, val=val)
+    @staticmethod
+    def create(key, val):
+        return client.CreateRecord(key=key, val=val)
 
+    def read(self, key):
+        return client.ReadRecord(key=key)
 
-def read(key):
-    return client.ReadRecord(key=key)
+    def update(self, key, val):
+        return client.UpdateRecord(key=key, val=val)
 
+    def delete(self, key):
+        return client.DeleteRecord(key=key)
 
-def update(key, val):
-    return client.UpdateRecord(key=key, val=val)
+    def show_commands(self):
+        print "Input number of command to execute"
+        print "1. Create"
+        print "2. Read"
+        print "3. Update"
+        print "4. Delete"
+        print "5. Exit"
+        print
 
+    def _key(self):
+        return [raw_input('Key: ')]
 
-def delete(key):
-    return client.DeleteRecord(key=key)
+    def _key_and_val(self):
+        key = self._key()
+        key.append(raw_input('Value: '))
+        return key
 
+    def test_connection(self):
+        result = self.testf(5).MultResult
+        return int(result)
 
-def show_commands():
-    print "Input number of command to execute"
-    print "1. Create"
-    print "2. Read"
-    print "3. Update"
-    print "4. Delete"
-    print "5. Exit"
-    print
+    def start_dialog(self):
+        while True:
+            print
+            self.show_commands()
+            response = "No response"
 
+            try:
+                operation = int(raw_input("Number: "))
+            except ValueError:
+                print("Invalid number")
+                continue
 
-def _key():
-    return [raw_input('Key: ')]
+            if 1 == operation or 3 == operation:
+                inp = self._key_and_val()
 
+                if 1 == operation:
+                    response = self.create(inp[0], inp[1])
+                if 3 == operation:
+                    response = self.update(inp[0], inp[1])
 
-def _key_and_val():
-    key = _key()
-    key.append(raw_input('Value: '))
-    return key
+            elif 2 == operation or 4 == operation:
+                inp = self._key()
 
+                if 2 == operation:
+                    response = self.read(inp[0])
+                if 4 == operation:
+                    response = self.delete(inp[0])
 
-result = testf(5).MultResult
-print int(result)
+            elif 5 == operation:
+                break
 
-while True:
-    print
-    show_commands()
-    response = "No response"
+            else:
+                print "Invalid number"
+                continue
 
-    try:
-        operation = int(raw_input("Number: "))
-    except ValueError:
-        print("Invalid number")
-        continue
+            print response.Result
 
-    if 1 == operation or 3 == operation:
-        inp = _key_and_val()
-
-        if 1 == operation:
-            response = create(inp[0], inp[1])
-        if 3 == operation:
-            response = update(inp[0], inp[1])
-
-    elif 2 == operation or 4 == operation:
-        inp = _key()
-
-        if 2 == operation:
-            response = read(inp[0])
-        if 4 == operation:
-            response = delete(inp[0])
-
-    elif 5 == operation:
-        break
-
-    else:
-        print "Invalid number"
-        continue
-
-    print response.Result
+if __name__ == '__main__':
+    clientClass = ClassProduction()
+    clientClass.start_dialog()
--- tests.py
+++ tests.py
@@ -2,10 +2,13 @@ import unittest
 from mock import *
 
 import Server
-# import Client
+import Client
+from Client import ClassProduction
+from mock import MagicMock
+from mock import patch
+
 
 class Test(unittest.TestCase):
-
     def test_create(self):
         Server.delete("h")
         self.assertEqual(Server.success, Server.create("h", "h"))
@@ -29,6 +32,14 @@ class Test(unittest.TestCase):
         self.assertEqual("Fail! Record with key \"h\" was not found!", Server.delete("h"))
         Server.create("h", "ha")
         self.assertEqual(Server.success, Server.delete("h"))
+
+    def test_c_create(self):
+        thing = ClassProduction
+        # thing.method = MagicMock(return_value="Success!")
+        with patch('Client.ClassProduction.create') as perm_mock:
+            assert isinstance(perm_mock, object)
+            perm_mock.return_value = "Success!"
+            self.assertEqual(thing.create("1", "1"), "Success!")
 
 
 if __name__ == '__main__':
